e569645816f3deb5376b277c665eac8bf0f2e3cb
tky_st_mac <tiankeyu@sensetime.com>
2019-11-25 17:38:49 +0800

    upd gitignore(remove *.log) create .log refactor search.py(C => Config)

--- FastAutoAugment/search.py
+++ FastAutoAugment/search.py
@@ -23,9 +23,10 @@ from FastAutoAugment.data import get_dataloaders
 from FastAutoAugment.metrics import Accumulator
 from FastAutoAugment.networks import get_model, num_class
 from FastAutoAugment.train import train_and_eval
-from theconf import Config as C, ConfigArgumentParser
+from theconf import Config, ConfigArgumentParser
 
 
+cuda_is_available = torch.cuda.is_available()
 top1_valid_by_cv = defaultdict(lambda: list)
 
 
@@ -59,24 +60,24 @@ def _get_path(dataset, model, tag):
 
 @ray.remote(num_gpus=4, max_calls=1)
 def train_model(config, dataroot, augment, cv_ratio_test, cv_fold, save_path=None, skip_exist=False):
-    C.get()
-    C.get().conf = config
-    C.get()['aug'] = augment
+    Config.get()
+    Config.get().conf = config
+    Config.get()['aug'] = augment
 
     result = train_and_eval(None, dataroot, cv_ratio_test, cv_fold, save_path=save_path, only_eval=skip_exist)
-    return C.get()['model']['type'], cv_fold, result
+    return Config.get()['model']['type'], cv_fold, result
 
 
 def eval_tta(config, augment, reporter):
-    C.get()
-    C.get().conf = config
+    Config.get()
+    Config.get().conf = config
     cv_ratio_test, cv_fold, save_path = augment['cv_ratio_test'], augment['cv_fold'], augment['save_path']
 
     # setup - provided augmentation rules
-    C.get()['aug'] = policy_decoder(augment, augment['num_policy'], augment['num_op'])
+    Config.get()['aug'] = policy_decoder(augment, augment['num_policy'], augment['num_op'])
 
     # eval
-    model = get_model(C.get()['model'], num_class(C.get()['dataset']))
+    model = get_model(Config.get()['model'], num_class(Config.get()['dataset']))
     ckpt = torch.load(save_path)
     if 'model' in ckpt:
         model.load_state_dict(ckpt['model'])
@@ -86,7 +87,7 @@ def eval_tta(config, augment, reporter):
 
     loaders = []
     for _ in range(augment['num_policy']):  # TODO
-        _, tl, validloader, tl2 = get_dataloaders(C.get()['dataset'], C.get()['batch'], augment['dataroot'], cv_ratio_test, split_idx=cv_fold)
+        _, tl, validloader, tl2 = get_dataloaders(Config.get()['dataset'], Config.get()['batch'], augment['dataroot'], cv_ratio_test, split_idx=cv_fold)
         loaders.append(iter(validloader))
         del tl, tl2
 
@@ -99,8 +100,9 @@ def eval_tta(config, augment, reporter):
             corrects = []
             for loader in loaders:
                 data, label = next(loader)
-                data = data.cuda()
-                label = label.cuda()
+                if cuda_is_available:
+                    data = data.cuda()
+                    label = label.cuda()
 
                 pred = model(data)
 
@@ -129,7 +131,7 @@ def eval_tta(config, augment, reporter):
 
     del model
     metrics = metrics / 'cnt'
-    gpu_secs = (time.time() - start_t) * torch.cuda.device_count()
+    gpu_secs = (time.time() - start_t) * (torch.cuda.device_count() if cuda_is_available else 1)
     reporter(minus_loss=metrics['minus_loss'], top1_valid=metrics['correct'], elapsed_time=gpu_secs, done=True)
     return metrics['correct']
 
@@ -155,28 +157,28 @@ if __name__ == '__main__':
 
     if args.decay > 0:
         logger.info('decay=%.4f' % args.decay)
-        C.get()['optimizer']['decay'] = args.decay
+        Config.get()['optimizer']['decay'] = args.decay
 
-    add_filehandler(logger, os.path.join('models', '%s_%s_cv%.1f.log' % (C.get()['dataset'], C.get()['model']['type'], args.cv_ratio)))
+    add_filehandler(logger, os.path.join('models', '%s_%s_cv%.1f.log' % (Config.get()['dataset'], Config.get()['model']['type'], args.cv_ratio)))
     logger.info('configuration...')
-    logger.info(json.dumps(C.get().conf, sort_keys=True, indent=4))
+    logger.info(json.dumps(Config.get().conf, sort_keys=True, indent=4))
     logger.info('initialize ray...')
     ray.init(redis_address=args.redis)
 
     num_result_per_cv = 10
     cv_num = 5
-    copied_c = copy.deepcopy(C.get().conf)
+    copied_c = copy.deepcopy(Config.get().conf)
 
-    logger.info('search augmentation policies, dataset=%s model=%s' % (C.get()['dataset'], C.get()['model']['type']))
+    logger.info('search augmentation policies, dataset=%s model=%s' % (Config.get()['dataset'], Config.get()['model']['type']))
     logger.info('----- Train without Augmentations cv=%d ratio(test)=%.1f -----' % (cv_num, args.cv_ratio))
     w.start(tag='train_no_aug')
-    paths = [_get_path(C.get()['dataset'], C.get()['model']['type'], 'ratio%.1f_fold%d' % (args.cv_ratio, i)) for i in range(cv_num)]
+    paths = [_get_path(Config.get()['dataset'], Config.get()['model']['type'], 'ratio%.1f_fold%d' % (args.cv_ratio, i)) for i in range(cv_num)]
     print(paths)
     reqs = [
-        train_model.remote(copy.deepcopy(copied_c), args.dataroot, C.get()['aug'], args.cv_ratio, i, save_path=paths[i], skip_exist=True)
+        train_model.remote(copy.deepcopy(copied_c), args.dataroot, Config.get()['aug'], args.cv_ratio, i, save_path=paths[i], skip_exist=True)
         for i in range(cv_num)]
 
-    tqdm_epoch = tqdm(range(C.get()['epoch']))
+    tqdm_epoch = tqdm(range(Config.get()['epoch']))
     is_done = False
     for epoch in tqdm_epoch:
         while True:
@@ -185,13 +187,13 @@ if __name__ == '__main__':
                 try:
                     latest_ckpt = torch.load(paths[cv_idx])
                     if 'epoch' not in latest_ckpt:
-                        epochs_per_cv['cv%d' % (cv_idx + 1)] = C.get()['epoch']
+                        epochs_per_cv['cv%d' % (cv_idx + 1)] = Config.get()['epoch']
                         continue
                     epochs_per_cv['cv%d' % (cv_idx+1)] = latest_ckpt['epoch']
                 except Exception as e:
                     continue
             tqdm_epoch.set_postfix(epochs_per_cv)
-            if len(epochs_per_cv) == cv_num and min(epochs_per_cv.values()) >= C.get()['epoch']:
+            if len(epochs_per_cv) == cv_num and min(epochs_per_cv.values()) >= Config.get()['epoch']:
                 is_done = True
             if len(epochs_per_cv) == cv_num and min(epochs_per_cv.values()) >= epoch:
                 break
@@ -224,7 +226,7 @@ if __name__ == '__main__':
     reward_attr = 'top1_valid'      # top1_valid or minus_loss
     for _ in range(1):  # run multiple times.
         for cv_fold in range(cv_num):
-            name = "search_%s_%s_fold%d_ratio%.1f" % (C.get()['dataset'], C.get()['model']['type'], cv_fold, args.cv_ratio)
+            name = "search_%s_%s_fold%d_ratio%.1f" % (Config.get()['dataset'], Config.get()['model']['type'], cv_fold, args.cv_ratio)
             print(name)
             register_trainable(name, lambda augs, rpt: eval_tta(copy.deepcopy(copied_c), augs, rpt))
             algo = HyperOptSearch(space, max_concurrent=4*20, reward_attr=reward_attr)
@@ -261,16 +263,16 @@ if __name__ == '__main__':
     logger.info(json.dumps(final_policy_set))
     logger.info('final_policy=%d' % len(final_policy_set))
     logger.info('processed in %.4f secs, gpu hours=%.4f' % (w.pause('search'), total_computation / 3600.))
-    logger.info('----- Train with Augmentations model=%s dataset=%s aug=%s ratio(test)=%.1f -----' % (C.get()['model']['type'], C.get()['dataset'], C.get()['aug'], args.cv_ratio))
+    logger.info('----- Train with Augmentations model=%s dataset=%s aug=%s ratio(test)=%.1f -----' % (Config.get()['model']['type'], Config.get()['dataset'], Config.get()['aug'], args.cv_ratio))
     w.start(tag='train_aug')
 
     num_experiments = 5
-    default_path = [_get_path(C.get()['dataset'], C.get()['model']['type'], 'ratio%.1f_default%d' % (args.cv_ratio, _)) for _ in range(num_experiments)]
-    augment_path = [_get_path(C.get()['dataset'], C.get()['model']['type'], 'ratio%.1f_augment%d' % (args.cv_ratio, _)) for _ in range(num_experiments)]
-    reqs = [train_model.remote(copy.deepcopy(copied_c), args.dataroot, C.get()['aug'], 0.0, 0, save_path=default_path[_], skip_exist=True) for _ in range(num_experiments)] + \
-        [train_model.remote(copy.deepcopy(copied_c), args.dataroot, final_policy_set, 0.0, 0, save_path=augment_path[_]) for _ in range(num_experiments)]
+    default_path = [_get_path(Config.get()['dataset'], Config.get()['model']['type'], 'ratio%.1f_default%d' % (args.cv_ratio, _)) for _ in range(num_experiments)]
+    augment_path = [_get_path(Config.get()['dataset'], Config.get()['model']['type'], 'ratio%.1f_augment%d' % (args.cv_ratio, _)) for _ in range(num_experiments)]
+    reqs = [train_model.remote(copy.deepcopy(copied_c), args.dataroot, Config.get()['aug'], 0.0, 0, save_path=default_path[_], skip_exist=True) for _ in range(num_experiments)] + \
+           [train_model.remote(copy.deepcopy(copied_c), args.dataroot, final_policy_set, 0.0, 0, save_path=augment_path[_]) for _ in range(num_experiments)]
 
-    tqdm_epoch = tqdm(range(C.get()['epoch']))
+    tqdm_epoch = tqdm(range(Config.get()['epoch']))
     is_done = False
     for epoch in tqdm_epoch:
         while True:
@@ -290,7 +292,7 @@ if __name__ == '__main__':
                     pass
 
             tqdm_epoch.set_postfix(epochs)
-            if len(epochs) == num_experiments*2 and min(epochs.values()) >= C.get()['epoch']:
+            if len(epochs) == num_experiments*2 and min(epochs.values()) >= Config.get()['epoch']:
                 is_done = True
             if len(epochs) == num_experiments*2 and min(epochs.values()) >= epoch:
                 break
