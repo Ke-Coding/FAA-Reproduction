7d0ad5a2788476501a6c0966d2be6724a9fed080
tky_st_sh36 <tiankeyu@sensetime.com>
2019-11-27 16:12:21 +0800

    upd run.sh(using srun)

--- experiments/debug/run.sh
+++ experiments/debug/run.sh
@@ -1,4 +1,4 @@
 ROOT=../..
-
+srun --mpi=pmi2 -p $1 -n1 --gres=gpu:8 --ntasks-per-node=8 --cpus-per-task=5 --job-name=FastAA-CF10 \
 python -u $ROOT/search.py -c $ROOT/confs/wresnet40x2_cifar10_b512.yaml \
  #--recover=checkpoints/ckpt_1000.pth.tar \
