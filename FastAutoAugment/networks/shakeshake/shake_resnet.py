85b8a397b4d03247b9e5e179623c92d7890b1861
tky_st_mac <tiankeyu@sensetime.com>
2019-12-02 16:09:54 +0800

    remove inplace in shake_resnet

--- FastAutoAugment/networks/shakeshake/shake_resnet.py
+++ FastAutoAugment/networks/shakeshake/shake_resnet.py
@@ -28,10 +28,10 @@ class ShakeBlock(nn.Module):
 
     def _make_branch(self, in_ch, out_ch, stride=1):
         return nn.Sequential(
-            nn.ReLU(inplace=True),
+            nn.ReLU(inplace=False),
             nn.Conv2d(in_ch, out_ch, 3, padding=1, stride=stride, bias=False),
             nn.BatchNorm2d(out_ch),
-            nn.ReLU(inplace=True),
+            nn.ReLU(inplace=False),
             nn.Conv2d(out_ch, out_ch, 3, padding=1, stride=1, bias=False),
             nn.BatchNorm2d(out_ch))
 
