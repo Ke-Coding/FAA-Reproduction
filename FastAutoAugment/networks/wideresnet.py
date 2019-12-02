0a9752125ed35a3df48135cb2d6253649c652f4a
tky_st_mac <tiankeyu@sensetime.com>
2019-12-02 16:06:37 +0800

    refactor nets

--- FastAutoAugment/networks/wideresnet.py
+++ FastAutoAugment/networks/wideresnet.py
@@ -19,20 +19,20 @@ def conv_init(m):
 
 
 class WideBasic(nn.Module):
-    def __init__(self, in_planes, planes, dropout_rate, stride=1):
+    def __init__(self, inplanes, outplanes, dropout_rate, stride=1):
         super(WideBasic, self).__init__()
-        self.bn1 = nn.BatchNorm2d(in_planes, momentum=0.9)
-        self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=3, padding=1, bias=True)
+        self.bn1 = nn.BatchNorm2d(inplanes, momentum=0.9)
+        self.conv1 = nn.Conv2d(inplanes, outplanes, kernel_size=3, padding=1, bias=True)
         self.using_dropout = dropout_rate is not None
         if self.using_dropout:
             self.dropout = nn.Dropout(p=dropout_rate)
-        self.bn2 = nn.BatchNorm2d(planes, momentum=0.9)
-        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=stride, padding=1, bias=True)
+        self.bn2 = nn.BatchNorm2d(outplanes, momentum=0.9)
+        self.conv2 = nn.Conv2d(outplanes, outplanes, kernel_size=3, stride=stride, padding=1, bias=True)
 
-        self.is_identity = stride == 1 and in_planes == planes
+        self.is_identity = stride == 1 and inplanes == outplanes
         if not self.is_identity:
             self.shortcut = nn.Sequential(
-                nn.Conv2d(in_planes, planes, kernel_size=1, stride=stride, bias=True),
+                nn.Conv2d(inplanes, outplanes, kernel_size=1, stride=stride, bias=True),
             )
 
     def forward(self, x):
@@ -51,17 +51,16 @@ class WideResNet(nn.Module):
         self.in_planes = 16
 
         assert ((depth - 4) % 6 == 0), 'Wide-resnet depth should be 6n+4'
-        n = int((depth - 4) / 6)
-        k = widen_factor
+        n = (depth - 4) // 6
 
-        nStages = [16, 16*k, 32*k, 64*k]
+        ch = [16, 16*widen_factor, 32*widen_factor, 64*widen_factor]
 
-        self.conv1 = conv3x3(3, nStages[0])
-        self.layer1 = self._wide_layer(WideBasic, nStages[1], n, dropout_rate, stride=1)
-        self.layer2 = self._wide_layer(WideBasic, nStages[2], n, dropout_rate, stride=2)
-        self.layer3 = self._wide_layer(WideBasic, nStages[3], n, dropout_rate, stride=2)
-        self.bn1 = nn.BatchNorm2d(nStages[3], momentum=0.9)
-        self.linear = nn.Linear(nStages[3], num_classes)
+        self.conv1 = conv3x3(3, ch[0])
+        self.layer1 = self._wide_layer(WideBasic, ch[1], n, dropout_rate, stride=1)
+        self.layer2 = self._wide_layer(WideBasic, ch[2], n, dropout_rate, stride=2)
+        self.layer3 = self._wide_layer(WideBasic, ch[3], n, dropout_rate, stride=2)
+        self.bn1 = nn.BatchNorm2d(ch[3], momentum=0.9)
+        self.linear = nn.Linear(ch[3], num_classes, bias=True)
 
         # self.apply(conv_init)
 
